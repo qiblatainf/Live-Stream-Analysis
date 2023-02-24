eca6acbcab7fa274f5c63c9c9a54b035fdb7b68e
qiblatainf <qiblatain@live.com>
2023-02-24 22:38:42 +0500

    Web Socket added

--- bot/bot.js
+++ bot/bot.js
@@ -2,6 +2,22 @@ url = "wss://sadiq.livekit.cloud"
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tIjoic3RyZWFtZXIiLCJyb29tSm9pbiI6dHJ1ZSwiY2FuU3Vic2NyaWJlIjp0cnVlLCJjYW5QdWJsaXNoIjpmYWxzZSwiY2FuUHVibGlzaERhdGEiOmZhbHNlfSwiaWF0IjoxNjc2OTkxMDE0LCJuYmYiOjE2NzY5OTEwMTQsImV4cCI6MTY3NzAxMjYxNCwiaXNzIjoiQVBJY0ZnRHBMdUEyOVRnIiwic3ViIjoidmlld2VyIiwianRpIjoidmlld2VyIn0.q9JDNxdbWpPqRgk5xcFl2D1hIGVQBwB5ID-XhLnWx9k"
 
 const livekit = require('livekit-client');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const WebSocket = require('ws');
+
+// sets up web environment
+const dom = new JSDOM();
+global.window = dom.window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+  // mediaDevices: {
+  //   getUserMedia: async function(constraints) {
+      
+  //   }
+  // }
+};
 // const room = new livekit.Room(...);
 // await room.connect(...);
 
@@ -19,13 +35,17 @@ const room = new livekit.Room({
 },
 });
 
-if (typeof window !== undefined) {
-    console.log('You are on the browser,You are good to go')
-    } else {
-    console.log('You are on the server,Cannot execute')
-   } // It will always go in else block
 // connect to room
-room.connect(url, token);
+const ws = new WebSocket(url);
+ws.on('open', () => {
+  console.log('connected to room');
+  try {
+    room.connect(ws, token);
+  }
+  catch(error) {
+    console.err(error);
+  }
+});
 
 // console.log('connected to room', room.name);
   
